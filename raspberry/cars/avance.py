287922e8535e65629e616a8ac1f463dfc611253c
Dimitri Fagart <dimi@schugart.net>
2014-07-09 20:55:07 +0200

    identation corrected

--- raspberry/cars/avance.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import memcache,time
-
-shared = memcache.Client(['127.0.0.1:11211'], debug=0)
-shared.set('Way', shared.get('neutralESC')+6)
-time.sleep(0.6)
-shared.set('Way', shared.get('neutralESC'))
