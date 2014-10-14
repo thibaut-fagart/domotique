bb6d68187968a9d591f8bfc25b87b497887b1d30
Dimitri Fagart <dimi@schugart.net>
2014-10-14 22:33:09 +0200

    relay snmp cave

--- python/audio_packet_check.py
+++ python/audio_packet_check.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
-
-# This needs to be run as root.
-
 import time
 import pcapy
-import sys
 import impacket.ImpactDecoder as Decoders
 import impacket.ImpactPacket as Packets
 from pysnmp.entity.rfc3413.oneliner import cmdgen
@@ -17,28 +13,31 @@ ipSqueezSalon   = "192.168.0.6"
 ipSqueezSdb     = "192.168.0.7"
 ipSqueezCuisine = "192.168.0.8"
 oidAmpliSalonState   = "1.3.6.1.4.1.43689.1.1.1.0"
-oidAmpliSdBState     = "1.3.6.1.4.1.43689.1.1.2.0"
+oidAmpliSdbState     = "1.3.6.1.4.1.43689.1.1.2.0"
 oidAmpliCuisineState = "1.3.6.1.4.1.43689.1.1.3.0"
 oidAmpliChambreState = "1.3.6.1.4.1.43689.1.1.4.0"
 
-def printlog(text):
+def printlog(datelog,text):
   fileToBeWriten = "/home/dimi/prog/log/audio_packet_check_log.txt"
   fichierWrite = open(fileToBeWriten,'a')
+  fichierWrite.write(datelog)
   fichierWrite.write(text)
+  fichierWrite.write('\n')
   fichierWrite.close()  
 
-def set_ArduinoValue(Oid,ipHostSnmp,value):
+def set_Value(Oid,ipHostSnmp,value):
   errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CommandGenerator().setCmd(
       cmdgen.CommunityData('private',mpModel=0),
       cmdgen.UdpTransportTarget((ipHostSnmp, 161)),
       (Oid, rfc1902.Integer(value)))
+  printlog(time.asctime(),'set oid %s from %s to %s value \n'%(Oid,ipHostSnmp,value))
 
   # Check for errors and print out results
   if errorIndication:
-    printlog('get oid %s from %s errorIndication : %s'%(Oid,ipHostSnmp,errorIndication))
+    printlog(time.asctime(),'get oid %s from %s errorIndication : %s \n'%(Oid,ipHostSnmp,errorIndication))
   else:
     if errorStatus:
-      printlog('get oid %s from %s errorStatus : %s at %s'%(Oid,ipHostSnmp,errorStatus.prettyPrint(),errorIndex and varBinds[int(errorIndex)-1] or '?'))
+      printlog(time.asctime(),'get oid %s from %s errorStatus : %s at %s \n'%(Oid,ipHostSnmp,errorStatus.prettyPrint(),errorIndex and varBinds[int(errorIndex)-1] or '?'))
     else:
       for name, val in varBinds:
         #printlog('get oid %s from %s : %s = %s'%(Oid,ipHostSnmp,name.prettyPrint(), val.prettyPrint()))
@@ -47,35 +46,31 @@ def set_ArduinoValue(Oid,ipHostSnmp,value):
 def main():
   
   lastTime = 0
-  audioMaxDelay = 240
+  audioMaxDelay = 300
   audioCurrentDelay = 0
   deltaTime = 2
   audioChambrePower = 0
-  audioSdbPower = 0
   audioCuisinePower = 0
+  audioSalonPower   = 0
+  audioSdbPower     = 0
 
   lastPacketCuisine = int(time.time()) - audioMaxDelay
   lastPacketChambre = int(time.time()) - audioMaxDelay
-  lastPacketSdb     = int(time.time()) - audioMaxDelay
   lastPacketSalon   = int(time.time()) - audioMaxDelay
+  lastPacketSdb     = int(time.time()) - audioMaxDelay
 
-  print 'Opening eth0' 
+  print 'Opening eth0'
 
-  # Arguments here are:
-  #   device
-  #   snaplen (maximum number of bytes to capture _per_packet_)
-  #   promiscious mode (1 for true)
-  #   timeout (in milliseconds)
-  cap = pcapy.open_live('eth0', 5000, 1, 0)
-  #cap.setfilter('src host 192.168.0.103 and port 9000')
+  cap = pcapy.open_live('eth0', 5000, 1, 1000)
+  cap.setfilter('src host 192.168.0.103 and port 9000')
   eth_decoder = Decoders.EthDecoder()
   ip_decoder = Decoders.IPDecoder()
 
-  # Read packets -- header contains information about the data from pcap,
-  # payload is the actual packet as a string
-  (header, payload) = cap.next()
-  while (header):
-    (header, payload) = cap.next()
+  while (True):
+    try:
+      (header, payload) = cap.next()
+    except:
+      continue
 
     ethernet = eth_decoder.decode(payload)
     if ethernet.get_ether_type() == Packets.IP.ethertype:
@@ -87,35 +82,39 @@ def main():
           lastPacketCuisine = int(time.time())
         if (ip_src == ipSqueezCenter and ip_dst == ipSqueezChambre):
           lastPacketChambre = int(time.time())
-        if (ip_src == ipSqueezCenter and ip_dst == ipSqueezSdb):
-          lastPacketSdb = int(time.time())
         if (ip_src == ipSqueezCenter and ip_dst == ipSqueezSalon):
           lastPacketSalon = int(time.time())
+        if (ip_src == ipSqueezCenter and ip_dst == ipSqueezSdb):
+          lastPacketSdb = int(time.time())
 
 	if ((int(time.time()) - lastTime) > deltaTime):
 	# audioCurrentDelay indicate the time since the last packet received
             audioCurrentDelay = max((int(time.time()) - lastPacketChambre), audioCurrentDelay)  
             audioCurrentDelay = max((int(time.time()) - lastPacketCuisine), audioCurrentDelay)  
-            audioCurrentDelay = max((int(time.time()) - lastPacketSdb), audioCurrentDelay) 
+            audioCurrentDelay = max((int(time.time()) - lastPacketSalon)  , audioCurrentDelay) 
+            audioCurrentDelay = max((int(time.time()) - lastPacketSdb)    , audioCurrentDelay) 
 
             lastAudioChambrePower = audioChambrePower
             lastAudioCuisinePower = audioCuisinePower
+            lastAudioSalonPower   = audioSalonPower
             lastAudioSdbPower     = audioSdbPower
 
             audioChambrePower = ((int(time.time()) - lastPacketChambre) < audioMaxDelay)
             audioCuisinePower = ((int(time.time()) - lastPacketCuisine) < audioMaxDelay)
-            audioSdbPower     = ((int(time.time()) - lastPacketSdb) < audioMaxDelay)
+            audioSalonPower   = ((int(time.time()) - lastPacketSalon)   < audioMaxDelay)
+            audioSdbPower     = ((int(time.time()) - lastPacketSdb)     < audioMaxDelay)
 	
 	    if lastAudioCuisinePower != audioCuisinePower:
-                set_ArduinoValue(oidAmpliCuisineState,ipHostSnmp,audioCuisinePower)
-	    if lastAudioSdbPower != audioSdbPower:
-                set_ArduinoValue(oidAmpliSdBState,ipHostSnmp,audioSdbPower)
+                set_Value(oidAmpliCuisineState,ipHostSnmp,audioCuisinePower)
 	    if lastAudioChambrePower != audioChambrePower:
-                set_ArduinoValue(oidAmpliChambreState,ipHostSnmp,audioChambrePower)
-         # Le flux reseau n'est pas detecte entre la squeezebox et la sdb ???? -> couplage avec le salon
-                set_ArduinoValue(oidAmpliSdBState,ipHostSnmp,audioSdbPower)
+                set_Value(oidAmpliChambreState,ipHostSnmp,audioChambrePower)
+	    if lastAudioSalonPower != audioSalonPower:
+                set_Value(oidAmpliSalonState,ipHostSnmp,audioSalonPower)
+	    if lastAudioSdbPower != audioSdbPower:
+                set_Value(oidAmpliSdbState,ipHostSnmp,audioSdbPower)
 		
             lastTime = int(time.time())
 	
 if __name__ == "__main__":
+  time.sleep(30)
   main()
--- raspberry/dht/relayCave.py
+++ raspberry/dht/relayCave.py
@@ -3,7 +3,9 @@
 
 import sys
 from setsnmp import setSnmp
-from getsnmp import getSnmp
+
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+from pysnmp.proto import rfc1902
 
 oidArduinoCaveTemp = "1.3.6.1.4.1.36582.1"
 oidArduinoCaveHum  = "1.3.6.1.4.1.36582.2"
@@ -11,6 +13,28 @@ oidDht22CaveTemp   = "1.3.6.1.4.1.43689.1.2.8.1.0"
 oidDht22CaveHum    = "1.3.6.1.4.1.43689.1.2.8.2.0"
 ipHostCave        = "192.168.0.71"
 ipHostSnmp        = "192.168.0.110"
+
+def getSnmp(host,oid):
+  cmdGen = cmdgen.CommandGenerator()
+
+  errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+      cmdgen.CommunityData('public', mpModel=0),
+      cmdgen.UdpTransportTarget((host, 161)),
+      oid
+      )
+  # Check for errors and print out results
+  if errorIndication:
+      print(errorIndication)
+  else:
+      if errorStatus:
+          print('%s at %s' % (
+              errorStatus.prettyPrint(),
+              errorIndex and varBinds[int(errorIndex)-1] or '?'
+              )
+          )
+      else:
+          for name, val in varBinds:
+              return name.prettyPrint(), val.prettyPrint()
 
 if __name__ == "__main__":
 
