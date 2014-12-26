632f9ea94c505d8462f05a6f32531031b6afd2b3
Stéphane Bouey <stephane.bouey@faboba.com>
2014-12-26 15:27:12 +0100

    edf as class

--- raspberry/leroux/lerouxDht.py
+++ raspberry/leroux/lerouxDht.py
@@ -27,72 +27,130 @@ for pin in gpioBCMPrise3:
 
 ipHostSnmp            = "192.168.0.199"
 
-oidEdfAdco    = "1.3.6.1.4.1.43689.1.4.1.0"
-oidEdfOptarif = "1.3.6.1.4.1.43689.1.4.2.0"
-oidEdfIsousc  = "1.3.6.1.4.1.43689.1.4.3.0"
-oidEdfEjphn   = "1.3.6.1.4.1.43689.1.4.9.0"
-oidEdfEjphpm  = "1.3.6.1.4.1.43689.1.4.10.0"
-oidEdfPtec    = "1.3.6.1.4.1.43689.1.4.5.0"
-oidEdfIinst1  = "1.3.6.1.4.1.43689.1.4.11.0"
-oidEdfIinst2  = "1.3.6.1.4.1.43689.1.4.12.0"
-oidEdfIinst3  = "1.3.6.1.4.1.43689.1.4.13.0"
-oidEdfImax1   = "1.3.6.1.4.1.43689.1.4.14.0"
-oidEdfImax2   = "1.3.6.1.4.1.43689.1.4.15.0"
-oidEdfImax3   = "1.3.6.1.4.1.43689.1.4.16.0"
-oidEdfPmax    = "1.3.6.1.4.1.43689.1.4.17.0"
-oidEdfPapp    = "1.3.6.1.4.1.43689.1.4.8.0"
-ipHostSnmp    = "192.168.0.199"
 
-ser = serial.Serial(
-        port='/dev/ttyAMA0',
-        baudrate=1200,
-        parity=serial.PARITY_EVEN,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.SEVENBITS )
+class Edf:
+    oidEdfAdco = "1.3.6.1.4.1.43689.1.4.1.0"
+    oidEdfOptarif = "1.3.6.1.4.1.43689.1.4.2.0"
+    oidEdfIsousc = "1.3.6.1.4.1.43689.1.4.3.0"
+    oidEdfEjphn = "1.3.6.1.4.1.43689.1.4.9.0"
+    oidEdfEjphpm = "1.3.6.1.4.1.43689.1.4.10.0"
+    oidEdfPtec = "1.3.6.1.4.1.43689.1.4.5.0"
+    oidEdfIinst1 = "1.3.6.1.4.1.43689.1.4.11.0"
+    oidEdfIinst2 = "1.3.6.1.4.1.43689.1.4.12.0"
+    oidEdfIinst3 = "1.3.6.1.4.1.43689.1.4.13.0"
+    oidEdfImax1 = "1.3.6.1.4.1.43689.1.4.14.0"
+    oidEdfImax2 = "1.3.6.1.4.1.43689.1.4.15.0"
+    oidEdfImax3 = "1.3.6.1.4.1.43689.1.4.16.0"
+    oidEdfPmax = "1.3.6.1.4.1.43689.1.4.17.0"
+    oidEdfPapp = "1.3.6.1.4.1.43689.1.4.8.0"
+
+    def __init__(self):
+        self.ser = serial.Serial(
+            port='/dev/ttyAMA0',
+            baudrate=1200,
+            parity=serial.PARITY_EVEN,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.SEVENBITS)
+
+    def checksum(self, etiquette, valeur):
+        sum = 32
+        for c in etiquette: sum = sum + ord(c)
+        for c in valeur:        sum = sum + ord(c)
+        sum = (sum & 63) + 32
+        return chr(sum)
+
+    def LireTeleinfo(self):
+        # Attendre le debut du message
+        while self.ser.read(1) != chr(2): pass
+
+        message = ""
+        fin = False
+
+        while not fin:
+            char = self.ser.read(1)
+            if char != chr(2):
+                message = message + char
+            else:
+                fin = True
+
+        trames = [
+            trame.split(" ")
+            for trame in message.strip("\r\n\x03").split("\r\n")
+        ]
+        print trames
+
+        tramesValides = dict([
+            [trame[0], trame[1]]
+            for trame in trames
+            if (len(trame) == 3) and (self.checksum(trame[0], trame[1]) == trame[2])
+        ])
+
+        return tramesValides
 
 
-def checksum (etiquette, valeur):
-                sum = 32
-                for c in etiquette: sum = sum + ord(c)
-                for c in valeur:        sum = sum + ord(c)
-                sum = (sum & 63) + 32
-                return chr(sum)
+    def teleinfoEJP(self):
+        self.ser.flushInput()
+        tramesOk = self.LireTeleinfo()
+        print tramesOk
+        self.ser.close()
+        for etiquette in tramesOk:
+            # if etiquette ==  'ADCO':
+            #result = setSnmp(ipHostSnmp,oidEdfAdco,int(tramesOk[etiquette]))
+            #if etiquette ==  'OPTARIF':
+            #result = setSnmp(ipHostSnmp,oidEdfOptarif,int(tramesOk[etiquette]))
+            if etiquette == 'ISOUSC':
+                self.ISOUSC = tramesOk[etiquette]
+                #print 'ISOUSC ',tramesOk[etiquette]
+            if etiquette == 'EJPHN':
+                self.EJPHN = tramesOk[etiquette]
+                #print 'EJPHN ',tramesOk[etiquette]
+            if etiquette == 'EJPHPM':
+                self.EJPHPM = tramesOk[etiquette]
+                #print 'EJPHPM ',tramesOk[etiquette]
+                #if etiquette ==  'PTEC':
+                #result = setSnmp(ipHostSnmp,oidEdfPtec,int(tramesOk[etiquette]))
+            if etiquette == 'IINST1':
+                self.IINST1 = tramesOk[etiquette]
+                #print 'IINST1 ',tramesOk[etiquette]
+            if etiquette == 'IINST2':
+                self.IINST2 = tramesOk[etiquette]
+                #print 'IINST2 ',tramesOk[etiquette]
+            if etiquette == 'IINST3':
+                self.IINST3 = tramesOk[etiquette]
+                #print 'IINST3 ',tramesOk[etiquette]
+                #if etiquette ==  'IMAX1':
+                #result = setSnmp(ipHostSnmp,oidEdfImax1,int(tramesOk[etiquette]))
+                #if etiquette ==  'IMAX2':
+                #result = setSnmp(ipHostSnmp,oidEdfImax2,int(tramesOk[etiquette]))
+                #if etiquette ==  'IMAX3':
+                #result = setSnmp(ipHostSnmp,oidEdfImax3,int(tramesOk[etiquette]))
+                #if etiquette ==  'PMAX':
+                #result = setSnmp(ipHostSnmp,oidEdfPmax,int(tramesOk[etiquette]))
+            if etiquette == 'PAPP':
+                self.PAPP = tramesOk[etiquette]
+                #print 'PAPP ',tramesOk[etiquette]
 
+    def toSnmpIsousc(self):
+        return (self.oidEdfIsousc, rfc1902.Integer(self.ISOUSC))
 
-def LireTeleinfo ():
-                # Attendre le debut du message
-                while ser.read(1) != chr(2): pass
+    def toSnmpEjpHpm(self):
+        return (self.oidEdfEjphpm, rfc1902.Integer(self.EJPHPM))
 
-                message = ""
-                fin = False
+    def toSnmpEjpHn(self):
+        return (self.oidEdfEjphn, rfc1902.Integer(self.EJPHN))
 
-                while not fin:
-                        char = ser.read(1)
-                        if char != chr(2):
-                                message = message + char
-                        else:
-                                fin = True
+    def toSnmpIInst1(self):
+        return (self.oidEdfIinst1, rfc1902.Integer(self.IINST1))
 
-                trames = [
-                        trame.split(" ")
-                        for trame in message.strip("\r\n\x03").split("\r\n")
-                        ]
-                print trames
+    def toSnmpIInst2(self):
+        return (self.oidEdfIinst2, rfc1902.Integer(self.IINST2))
 
-                tramesValides = dict([
-                        [trame[0],trame[1]]
-                        for trame in trames
-                        if (len(trame) == 3) and (checksum(trame[0],trame[1]) == trame[2])
-                        ])
+    def toSnmpIInst3(self):
+        return (self.oidEdfIinst3, rfc1902.Integer(self.IINST3))
 
-                return tramesValides
+    def toSnmpPApp(self):
+        return (self.oidEdfPapp, rfc1902.Integer(self.PAPP))
 
-
-def teleinfoEJP():
-  ser.flushInput()
-  tramesOk = LireTeleinfo()
-  print tramesOk
-  ser.close()
 
 
 class SenseurDHT :
@@ -147,10 +205,9 @@ if __name__ == "__main__":
 
   fuel = Fuel()
   fuel.readFuelData()
-  print("fuel remaining %s burnt %s" % (fuel.fuelRemaining, fuel.fuelBurnt))
-  print ("fuel tuple oid %s, val %s" %(fuel.toSnmpBurningTime()))
-  print ("dht tuple oid %s, val %s" %(dic['Rasp'].toSnmpSetTemp()))
-  teleinfoEJP()
+  edf = Edf()
+  edf.teleinfoEJP()
+
   cmdGen = cmdgen.CommandGenerator()
 
   errorIndication, errorStatus, errorIndex, varBinds = cmdGen.setCmd(
